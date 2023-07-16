3818a4a06db30e50f6cfefed88b6145d4e643976
Robinho1997 <robin.kempen@gmx.de>
2023-07-16 19:31:13 +0200

    description component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeroComponent } from './hero/hero.component';
+import { DescriptionComponent } from './description/description.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent
+    HeroComponent,
+    DescriptionComponent
   ],
   imports: [
     BrowserModule
