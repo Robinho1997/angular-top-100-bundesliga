310928f8e86c8cdb1bb17cfb81ddee3659323541
Robinho1997 <robin.kempen@gmx.de>
2023-07-16 22:20:47 +0200

    add player stats to data & display stats

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroComponent } from './hero/hero.component';
 import { DescriptionComponent } from './description/description.component';
+import { PlayerCardComponent } from './player-card/player-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroComponent,
-    DescriptionComponent
+    DescriptionComponent,
+    PlayerCardComponent
   ],
   imports: [
     BrowserModule
