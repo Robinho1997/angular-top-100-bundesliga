48c2a8cee0c1b45c15b889bc1e2582ea45c5a446
Robinho1997 <robin.kempen@gmx.de>
2023-07-16 21:58:29 +0200

    player data

---
+++ src/assets/players.ts
@@ -0,0 +1,17 @@
+export const players = [
+  {
+    name: 'Niclas Füllkrug',
+    club: 'Werder Bremen',
+    text: 'Der vielleicht größte Aufstieg der Saison. Füllkrug ballerte sich nicht nur zu Deutschlands Stürmer Nummer eins und fuhr zur WM, sondern auch zur Torjägerkanone.',
+  },
+  {
+    name: ' Jude Bellingham',
+    club: 'Borussia Dortmund',
+    text: "An seine Fähigkeiten zwischen Defensive und Offensive kam wohl kein anderer in der Bundesliga heran. Der Engländer hat den berühmten nächsten Schritt gemacht. Zudem jüngster Kapitän in Dortmund's langer Geschichte.",
+  },
+  {
+    name: 'Christopher Nkunku',
+    club: 'RB Leipzig',
+    text: 'Der Franzose weckte in der entscheidenden Saisonphase beinahe bei jedem Ballkontakt den Eindruck, dass er die gegnerische Verteidigung im Alleingang verzweifeln lässt - und das gelang ihm in einigen Momenten auch. Bei einem Abschied würde die Bundesliga einen der - oder wenn nicht sogar den - besten Angreifer verlieren.',
+  },
+];
